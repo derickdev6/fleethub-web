246ec87861880ea72ebb377f59754edd79995da5
DerickMacbook <derickdev6@gmail.com>
2025-06-30 01:22:15 -0500

    Fix ( src/components/data-table ): Hotfix

--- src/components/data-table/DataTableRow.vue
+++ src/components/data-table/DataTableRow.vue
@@ -1,8 +1,5 @@
 <template>
-  <tr
-    data-slot="table-row"
-    class="border-b cursor-pointer transition-colors duration-150"
-  >
+  <tr data-slot="table-row" class="border-b transition-colors duration-150">
     <slot />
   </tr>
 </template>
